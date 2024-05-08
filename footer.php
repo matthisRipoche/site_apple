b157d0da0a02eed916b8a84add5827d35e0f2fc3
matthisripoche <matthisripoche@gmail.com>
2024-05-08 15:57:51 +0200

    footer

--- footer.php
+++ footer.php
@@ -1,3 +1,103 @@
+<footer>
+    <div class="wrapper">
+        <div class="links">
+            <div>
+                <ul>
+                    <li class="titlelinks">S’informer et acheter</li>
+                    <li><a href="#">Store</a></li>
+                    <li><a href="#">Mac</a></li>
+                    <li><a href="#">iPad</a></li>
+                    <li><a href="#">iPhone</a></li>
+                    <li><a href="#">Watch</a></li>
+                    <li><a href="#">AirPods</a></li>
+                    <li><a href="#">TV & Maison</a></li>
+                    <li><a href="#">AirTag</a></li>
+                    <li><a href="#">Accessoires</a></li>
+                    <li><a href="#">Cartes Cadeaux</a></li>
+                </ul>
+                <ul>
+                    <li class="titlelinks">Apple Cartes</li>
+                    <li><a href="#">Apple Pay</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li class="titlelinks">Compte</li>
+                    <li><a href="#">Gérer votre identifiant Apple</a></li>
+                    <li><a href="#">Compte Apple Store</a></li>
+                    <li><a href="#">iCloud.com</a></li>
+                </ul>
+                <ul>
+                    <li class="titlelinks">Divertissements</li>
+                    <li><a href="#">Apple One</a></li>
+                    <li><a href="#">Apple TV+</a></li>
+                    <li><a href="#">Apple Music</a></li>
+                    <li><a href="#">Apple Arcade</a></li>
+                    <li><a href="#">Apple Fitness+</a></li>
+                    <li><a href="#">Apple Podcasts</a></li>
+                    <li><a href="#">Apple Books</a></li>
+                    <li><a href="#">App Store</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li class="titlelinks">Apple Store</li>
+                    <li><a href="#">Trouver un Apple Store</a></li>
+                    <li><a href="#">Genius Bar</a></li>
+                    <li><a href="#">Today at Apple</a></li>
+                    <li><a href="#">Stage d’été Apple</a></li>
+                    <li><a href="#">App Apple Store</a></li>
+                    <li><a href="#">Produits reconditionnés et soldés</a></li>
+                    <li><a href="#">Apple Trade In</a></li>
+                    <li><a href="#">État de votre commande</a></li>
+                    <li><a href="#">Aide à l’achat</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li class="titlelinks">Pour les entreprises</li>
+                    <li><a href="#">Apple et les entreprises</a></li>
+                    <li><a href="#">Acheter pour votre entreprise</a></li>
+                </ul>
+                <ul>
+                    <li class="titlelinks">Pour l’Éducation</li>
+                    <li><a href="#">Apple et l’Éducation</a></li>
+                    <li><a href="#">Acheter pour l’université</a></li>
+                </ul>
+                <ul>
+                    <li class="titlelinks">Pour la santé</li>
+                    <li><a href="#">Apple dans le secteur de la santé</a></li>
+                    <li><a href="#">La santé sur Apple Watch</a></li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li class="titlelinks">Valeurs d’Apple</li>
+                    <li><a href="#">Accessibilité</a></li>
+                    <li><a href="#">Éducation</a></li>
+                    <li><a href="#">Environnement</a></li>
+                    <li><a href="#">Confidentialité</a></li>
+                    <li><a href="#">Responsabilité des fournisseurs</a></li>
+                </ul>
+                <ul>
+                    <li class="titlelinks">À propos d’Apple</li>
+                    <li><a href="#">Newsroom</a></li>
+                    <li><a href="#">Direction d’Apple</a></li>
+                    <li><a href="#">Travailler chez Apple</a></li>
+                    <li><a href="#">Garantie</a></li>
+                    <li><a href="#">Investisseurs</a></li>
+                    <li><a href="#">Éthique et conformité</a></li>
+                    <li><a href="#">Évènements</a></li>
+                    <li><a href="#">Création d’emplois en Europe</a></li>
+                    <li><a href="#">Contacter Apple</a></li>
+                </ul>
+            </div>
+        </div>
+        <div>
+
+        </div>
+    </div>
+</footer>
 </body>
 
 </html>
